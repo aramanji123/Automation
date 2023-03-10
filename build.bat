1453bcda301a54c53c37fc4f542544db6571218f
aramanji123 <aramanji123@gmail.com>
2023-03-10 20:18:48 +0530

    automation scripts updated

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 echo "hello world"
 
+<<<<<<< HEAD
 cd "C:\Repo\control_structures"
 call groovy if_else.groovy
+=======
+cd ..
+call groovy if_else.groovy
+>>>>>>> a034b348d3284c116450677fbda0619c8ef7f73f
