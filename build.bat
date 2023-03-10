112f7e694c1c59f856503f994ad84b22c1b79e0d
ANJI <113800925+aramanji123@users.noreply.github.com>
2023-03-10 20:26:57 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,14 +1,7 @@
 echo "hello world"
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 cd "C:\Repo\control_structures"
 call groovy if_else.groovy
-=======
-cd ..
-call groovy if_else.groovy
->>>>>>> a034b348d3284c116450677fbda0619c8ef7f73f
-=======
-cd ..
-call groovy if_else.groovy
->>>>>>> a034b348d3284c116450677fbda0619c8ef7f73f
+
+
